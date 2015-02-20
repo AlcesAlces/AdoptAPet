9fbde76f9d4d01a5fc14cde2a716848ef82eb673
Chris Migut <cmigut@mail.usf.edu>
2015-02-20 09:52:37 -0500

    Customer and Checkout userControl

---
+++ AdoptAPet/UserControls/UserControlCheckOut.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoptAPet.UserControls
+{
+    public partial class UserControlCheckOut : UserControl
+    {
+        public UserControlCheckOut()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            PrintDialog PrintDialog1 = new PrintDialog();
+            PrintDialog1.ShowDialog();
+        }
+
+        private void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
