f3493dd89e791a3fb5d8b141aa0524e182557d65
Chris Migut <cmigut@mail.usf.edu>
2015-02-19 19:12:10 -0500

    Animal Information panel
    Added a general panel that will be later updated to include more info on
    the animal.

---
+++ AdoptAPet/UserControlAnimalInformation.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace AdoptAPet
+{
+    partial class UserControlAnimalInformation
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControlAnimalInformation));
+            this.gbAnimale_Information = new System.Windows.Forms.GroupBox();
+            this.pbIsAdoptedPicture_Information = new System.Windows.Forms.PictureBox();
+            this.btnAdopt_Information = new System.Windows.Forms.Button();
+            this.txtAnimalDes_Information = new System.Windows.Forms.TextBox();
+            this.txtAnimalBreed_Information = new System.Windows.Forms.TextBox();
+            this.txtAnimalSpecies_Infomation = new System.Windows.Forms.TextBox();
+            this.txtAnimalName_Information = new System.Windows.Forms.TextBox();
+            this.lblAnimalBreed = new System.Windows.Forms.Label();
+            this.lblAnimalSpecies = new System.Windows.Forms.Label();
+            this.lblAnimalName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbAnimalLocation = new System.Windows.Forms.GroupBox();
+            this.lblAnimalStreetAddress = new System.Windows.Forms.Label();
+            this.lblAnimalCity = new System.Windows.Forms.Label();
+            this.lblAnimalState = new System.Windows.Forms.Label();
+            this.lblAnimalZipCode = new System.Windows.Forms.Label();
+            this.gbAnimalDescription = new System.Windows.Forms.GroupBox();
+            this.txtAnimalAddress = new System.Windows.Forms.TextBox();
+            this.txtAnimalCity = new System.Windows.Forms.TextBox();
+            this.txtAnimalState = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.gbAnimale_Information.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIsAdoptedPicture_Information)).BeginInit();
+            this.gbAnimalLocation.SuspendLayout();
+            this.gbAnimalDescription.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbAnimale_Information
+            // 
+            this.gbAnimale_Information.Controls.Add(this.gbAnimalDescription);
+            this.gbAnimale_Information.Controls.Add(this.gbAnimalLocation);
+            this.gbAnimale_Information.Controls.Add(this.textBox1);
+            this.gbAnimale_Information.Controls.Add(this.label1);
+            this.gbAnimale_Information.Controls.Add(this.pbIsAdoptedPicture_Information);
+            this.gbAnimale_Information.Controls.Add(this.btnAdopt_Information);
+            this.gbAnimale_Information.Controls.Add(this.txtAnimalBreed_Information);
+            this.gbAnimale_Information.Controls.Add(this.txtAnimalSpecies_Infomation);
+            this.gbAnimale_Information.Controls.Add(this.txtAnimalName_Information);
+            this.gbAnimale_Information.Controls.Add(this.lblAnimalBreed);
+            this.gbAnimale_Information.Controls.Add(this.lblAnimalSpecies);
+            this.gbAnimale_Information.Controls.Add(this.lblAnimalName);
+            this.gbAnimale_Information.Location = new System.Drawing.Point(0, 8);
+            this.gbAnimale_Information.Name = "gbAnimale_Information";
+            this.gbAnimale_Information.Size = new System.Drawing.Size(617, 352);
+            this.gbAnimale_Information.TabIndex = 11;
+            this.gbAnimale_Information.TabStop = false;
+            this.gbAnimale_Information.Text = "Animal Information";
+            // 
+            // pbIsAdoptedPicture_Information
+            // 
+            this.pbIsAdoptedPicture_Information.Image = ((System.Drawing.Image)(resources.GetObject("pbIsAdoptedPicture_Information.Image")));
+            this.pbIsAdoptedPicture_Information.Location = new System.Drawing.Point(6, 247);
+            this.pbIsAdoptedPicture_Information.Name = "pbIsAdoptedPicture_Information";
+            this.pbIsAdoptedPicture_Information.Size = new System.Drawing.Size(110, 95);
+            this.pbIsAdoptedPicture_Information.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIsAdoptedPicture_Information.TabIndex = 9;
+            this.pbIsAdoptedPicture_Information.TabStop = false;
+            // 
+            // btnAdopt_Information
+            // 
+            this.btnAdopt_Information.Location = new System.Drawing.Point(253, 298);
+            this.btnAdopt_Information.Name = "btnAdopt_Information";
+            this.btnAdopt_Information.Size = new System.Drawing.Size(75, 44);
+            this.btnAdopt_Information.TabIndex = 8;
+            this.btnAdopt_Information.Text = "Adopt";
+            this.btnAdopt_Information.UseVisualStyleBackColor = true;
+            // 
+            // txtAnimalDes_Information
+            // 
+            this.txtAnimalDes_Information.Enabled = false;
+            this.txtAnimalDes_Information.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalDes_Information.Location = new System.Drawing.Point(6, 19);
+            this.txtAnimalDes_Information.Multiline = true;
+            this.txtAnimalDes_Information.Name = "txtAnimalDes_Information";
+            this.txtAnimalDes_Information.Size = new System.Drawing.Size(265, 299);
+            this.txtAnimalDes_Information.TabIndex = 7;
+            // 
+            // txtAnimalBreed_Information
+            // 
+            this.txtAnimalBreed_Information.Enabled = false;
+            this.txtAnimalBreed_Information.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalBreed_Information.Location = new System.Drawing.Point(163, 84);
+            this.txtAnimalBreed_Information.Name = "txtAnimalBreed_Information";
+            this.txtAnimalBreed_Information.Size = new System.Drawing.Size(164, 29);
+            this.txtAnimalBreed_Information.TabIndex = 6;
+            // 
+            // txtAnimalSpecies_Infomation
+            // 
+            this.txtAnimalSpecies_Infomation.Enabled = false;
+            this.txtAnimalSpecies_Infomation.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalSpecies_Infomation.Location = new System.Drawing.Point(163, 53);
+            this.txtAnimalSpecies_Infomation.Name = "txtAnimalSpecies_Infomation";
+            this.txtAnimalSpecies_Infomation.Size = new System.Drawing.Size(164, 29);
+            this.txtAnimalSpecies_Infomation.TabIndex = 5;
+            // 
+            // txtAnimalName_Information
+            // 
+            this.txtAnimalName_Information.Enabled = false;
+            this.txtAnimalName_Information.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalName_Information.Location = new System.Drawing.Point(163, 22);
+            this.txtAnimalName_Information.Name = "txtAnimalName_Information";
+            this.txtAnimalName_Information.Size = new System.Drawing.Size(164, 29);
+            this.txtAnimalName_Information.TabIndex = 4;
+            // 
+            // lblAnimalBreed
+            // 
+            this.lblAnimalBreed.AutoSize = true;
+            this.lblAnimalBreed.Location = new System.Drawing.Point(8, 89);
+            this.lblAnimalBreed.Name = "lblAnimalBreed";
+            this.lblAnimalBreed.Size = new System.Drawing.Size(108, 20);
+            this.lblAnimalBreed.TabIndex = 2;
+            this.lblAnimalBreed.Text = "Animal Breed:";
+            // 
+            // lblAnimalSpecies
+            // 
+            this.lblAnimalSpecies.AutoSize = true;
+            this.lblAnimalSpecies.Location = new System.Drawing.Point(8, 56);
+            this.lblAnimalSpecies.Name = "lblAnimalSpecies";
+            this.lblAnimalSpecies.Size = new System.Drawing.Size(122, 20);
+            this.lblAnimalSpecies.TabIndex = 1;
+            this.lblAnimalSpecies.Text = "Animal Species:";
+            // 
+            // lblAnimalName
+            // 
+            this.lblAnimalName.AutoSize = true;
+            this.lblAnimalName.Location = new System.Drawing.Point(8, 25);
+            this.lblAnimalName.Name = "lblAnimalName";
+            this.lblAnimalName.Size = new System.Drawing.Size(107, 20);
+            this.lblAnimalName.TabIndex = 0;
+            this.lblAnimalName.Text = "Animal Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(163, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 29);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Animal Age:";
+            // 
+            // gbAnimalLocation
+            // 
+            this.gbAnimalLocation.Controls.Add(this.textBox5);
+            this.gbAnimalLocation.Controls.Add(this.txtAnimalState);
+            this.gbAnimalLocation.Controls.Add(this.txtAnimalCity);
+            this.gbAnimalLocation.Controls.Add(this.txtAnimalAddress);
+            this.gbAnimalLocation.Controls.Add(this.lblAnimalZipCode);
+            this.gbAnimalLocation.Controls.Add(this.lblAnimalState);
+            this.gbAnimalLocation.Controls.Add(this.lblAnimalCity);
+            this.gbAnimalLocation.Controls.Add(this.lblAnimalStreetAddress);
+            this.gbAnimalLocation.Location = new System.Drawing.Point(0, 150);
+            this.gbAnimalLocation.Name = "gbAnimalLocation";
+            this.gbAnimalLocation.Size = new System.Drawing.Size(327, 91);
+            this.gbAnimalLocation.TabIndex = 12;
+            this.gbAnimalLocation.TabStop = false;
+            this.gbAnimalLocation.Text = "Animal Location";
+            // 
+            // lblAnimalStreetAddress
+            // 
+            this.lblAnimalStreetAddress.AutoSize = true;
+            this.lblAnimalStreetAddress.Location = new System.Drawing.Point(12, 24);
+            this.lblAnimalStreetAddress.Name = "lblAnimalStreetAddress";
+            this.lblAnimalStreetAddress.Size = new System.Drawing.Size(124, 20);
+            this.lblAnimalStreetAddress.TabIndex = 0;
+            this.lblAnimalStreetAddress.Text = "Street Address: ";
+            // 
+            // lblAnimalCity
+            // 
+            this.lblAnimalCity.AutoSize = true;
+            this.lblAnimalCity.Location = new System.Drawing.Point(12, 59);
+            this.lblAnimalCity.Name = "lblAnimalCity";
+            this.lblAnimalCity.Size = new System.Drawing.Size(43, 20);
+            this.lblAnimalCity.TabIndex = 1;
+            this.lblAnimalCity.Text = "City: ";
+            // 
+            // lblAnimalState
+            // 
+            this.lblAnimalState.AutoSize = true;
+            this.lblAnimalState.Location = new System.Drawing.Point(137, 59);
+            this.lblAnimalState.Name = "lblAnimalState";
+            this.lblAnimalState.Size = new System.Drawing.Size(52, 20);
+            this.lblAnimalState.TabIndex = 2;
+            this.lblAnimalState.Text = "State:";
+            // 
+            // lblAnimalZipCode
+            // 
+            this.lblAnimalZipCode.AutoSize = true;
+            this.lblAnimalZipCode.Location = new System.Drawing.Point(223, 59);
+            this.lblAnimalZipCode.Name = "lblAnimalZipCode";
+            this.lblAnimalZipCode.Size = new System.Drawing.Size(35, 20);
+            this.lblAnimalZipCode.TabIndex = 3;
+            this.lblAnimalZipCode.Text = "Zip:";
+            // 
+            // gbAnimalDescription
+            // 
+            this.gbAnimalDescription.Controls.Add(this.txtAnimalDes_Information);
+            this.gbAnimalDescription.Location = new System.Drawing.Point(334, 22);
+            this.gbAnimalDescription.Name = "gbAnimalDescription";
+            this.gbAnimalDescription.Size = new System.Drawing.Size(277, 324);
+            this.gbAnimalDescription.TabIndex = 13;
+            this.gbAnimalDescription.TabStop = false;
+            this.gbAnimalDescription.Text = "Animal Description";
+            // 
+            // txtAnimalAddress
+            // 
+            this.txtAnimalAddress.Enabled = false;
+            this.txtAnimalAddress.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalAddress.Location = new System.Drawing.Point(129, 19);
+            this.txtAnimalAddress.Name = "txtAnimalAddress";
+            this.txtAnimalAddress.Size = new System.Drawing.Size(192, 29);
+            this.txtAnimalAddress.TabIndex = 14;
+            // 
+            // txtAnimalCity
+            // 
+            this.txtAnimalCity.Enabled = false;
+            this.txtAnimalCity.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalCity.Location = new System.Drawing.Point(48, 54);
+            this.txtAnimalCity.Name = "txtAnimalCity";
+            this.txtAnimalCity.Size = new System.Drawing.Size(88, 29);
+            this.txtAnimalCity.TabIndex = 15;
+            // 
+            // txtAnimalState
+            // 
+            this.txtAnimalState.Enabled = false;
+            this.txtAnimalState.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnimalState.Location = new System.Drawing.Point(186, 54);
+            this.txtAnimalState.Name = "txtAnimalState";
+            this.txtAnimalState.Size = new System.Drawing.Size(37, 29);
+            this.txtAnimalState.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(253, 54);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(68, 29);
+            this.textBox5.TabIndex = 17;
+            // 
+            // UserControlAnimalInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbAnimale_Information);
+            this.Name = "UserControlAnimalInformation";
+            this.Size = new System.Drawing.Size(620, 360);
+            this.gbAnimale_Information.ResumeLayout(false);
+            this.gbAnimale_Information.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIsAdoptedPicture_Information)).EndInit();
+            this.gbAnimalLocation.ResumeLayout(false);
+            this.gbAnimalLocation.PerformLayout();
+            this.gbAnimalDescription.ResumeLayout(false);
+            this.gbAnimalDescription.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbAnimale_Information;
+        private System.Windows.Forms.PictureBox pbIsAdoptedPicture_Information;
+        private System.Windows.Forms.Button btnAdopt_Information;
+        private System.Windows.Forms.TextBox txtAnimalDes_Information;
+        private System.Windows.Forms.TextBox txtAnimalBreed_Information;
+        private System.Windows.Forms.TextBox txtAnimalSpecies_Infomation;
+        private System.Windows.Forms.TextBox txtAnimalName_Information;
+        private System.Windows.Forms.Label lblAnimalBreed;
+        private System.Windows.Forms.Label lblAnimalSpecies;
+        private System.Windows.Forms.Label lblAnimalName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbAnimalLocation;
+        private System.Windows.Forms.GroupBox gbAnimalDescription;
+        private System.Windows.Forms.Label lblAnimalZipCode;
+        private System.Windows.Forms.Label lblAnimalState;
+        private System.Windows.Forms.Label lblAnimalCity;
+        private System.Windows.Forms.Label lblAnimalStreetAddress;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtAnimalState;
+        private System.Windows.Forms.TextBox txtAnimalCity;
+        private System.Windows.Forms.TextBox txtAnimalAddress;
+    }
+}
---
+++ AdoptAPet/UserControlAnimalInformation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoptAPet
+{
+    public partial class UserControlAnimalInformation : UserControl
+    {
+        public UserControlAnimalInformation()
+        {
+            InitializeComponent();
+        }
+
+        public void setTxtAnimalName(string toSet)
+        {
+            txtAnimalName_Information.Text = toSet;
+        }
+        public void setTxtAnimalSpecies(string toSet)
+        {
+            txtAnimalSpecies_Infomation.Text = toSet;
+        }
+        public void setTxtAnimalBreed(string toSet)
+        {
+            txtAnimalBreed_Information.Text = toSet;
+        }
+        public void setTextAnimalDescription(string toSet)
+        {
+            //may have to format this string 
+            txtAnimalDes_Information.Text = toSet;
+        }
+        public void setAnimalAddress(string setStreet, string setCity, string setZip)
+        {
+            lblAnimalStreetAddress.Text = setStreet;
+            lblAnimalCity.Text = setCity;
+            lblAnimalZipCode.Text = setZip;
+        }
+    }
+}
